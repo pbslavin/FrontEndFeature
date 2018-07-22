bca5be9e9ea6e6bd3bfd166e19a3983e1a0d20e2
Peter Slavin <pbslavin@gmail.com>
2018-07-21 20:39:32 -0500

    Add project files.

---
+++ FrontEndFeature/Controllers/MyFormController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FrontEndFeature.Models;
+
+namespace FrontEndFeature.Controllers
+{
+    public class MyFormController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FormDataModel formDataModel)
+        {
+            Repository.AddFormDataObject(formDataModel);
+            return View("Results", Repository.FormDataObjects);
+        }
+    }
+}
---
+++ FrontEndFeature/Controllers/MyIndexController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEndFeature.Controllers
+{
+    public class MyIndexController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
