b469dbae9e29b75e6c148c4baa490ae683cf3053
Peter Slavin <pbslavin@gmail.com>
2018-07-24 20:05:36 -0500

    edited a couple of views

---
+++ FrontEndFeature/Models/TagHelpers/DisableAutocompleteTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FrontEndFeature.Models.TagHelpers
+{
+    public class DisableAutocompleteTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("Autocomplete", "off");
+        }
+    }
+}
---
+++ FrontEndFeature/Models/TagHelpers/WeekDayTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FrontEndFeature.Models.TagHelpers
+{
+    public class WeekDayTagHelper : TagHelper
+    {
+        public static string dayOfWeek
+        {
+            get
+            {
+                return DateTime.Today.DayOfWeek.ToString();
+            }
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string outputString = $"Today is {dayOfWeek}.";
+            output.TagName = "h3";
+            output.Content.SetContent(outputString);
+        }
+    }
+}
