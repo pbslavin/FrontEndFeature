b10957373c7edec58150bcc09db687392bd71647
Peter Slavin <pbslavin@gmail.com>
2018-07-25 11:59:37 -0500

    added an arguably functional, but non-optimal, version of DisableAutocompleteTagHelper

--- FrontEndFeature/Models/TagHelpers/DisableAutocompleteTagHelper.cs
+++ FrontEndFeature/Models/TagHelpers/DisableAutocompleteTagHelper.cs
@@ -6,11 +6,12 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace FrontEndFeature.Models.TagHelpers
 {
+    [HtmlTargetElement("input")]
     public class DisableAutocompleteTagHelper : TagHelper
     {
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            output.Attributes.SetAttribute("Autocomplete", "off");
+            output.Attributes.SetAttribute("autocomplete", "off");
         }
     }
 }
